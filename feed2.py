373aa344d9ac0aa73be968628497b8ca673fcede
Richard Marko <rmarko@fedoraproject.org>
2013-05-26 19:50:29 +0200

    add graphers

---
+++ feed2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import math
+
+import rostock
+machine = rostock.Rostock()
+dry = not True
+if dry:
+    machine.dry_run = True
+else:
+    machine.connect()
+machine.init()
+
+t = 0
+x = 0
+y = 0
+y = 0
+scale = 50
+zoffset = 20
+STEP = 0.1
+exp = 3
+up = True
+try:
+    while True:
+        t += STEP
+        # y = 5 * math.sin(x/4) * math.cos(x/8)
+        # y = -3 * math.sin(x/3) + 5 * math.cos(x/7)
+        x = scale * math.sin(t)**exp
+        # y = 5 * math.sin(x / 4)
+        y = scale * math.cos(t)**exp
+        z =  scale * math.sin(t/10) + zoffset
+        #z = zoffset
+        machine.send('G1 X{0} Y{1} Z{2}'.format(x, y, z))
+        #machine.send('G1 Y{0}'.format(y))
+
+except KeyboardInterrupt:
+    pass
+
+machine.send('G1 X0 Y0 Z180')
+machine.end()
